9b0711b1acbdf428d82ec31aad3ddfcae7be75fc
Lars Langhans <lars.langhans@gmx.de>
2016-12-01 11:13:47 +0100

    Add more parts

--- src/main/java/org/homenet/moonserver/PostFile.java
+++ src/main/java/org/homenet/moonserver/PostFile.java
@@ -1,6 +1,7 @@
 package org.homenet.moonserver;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import org.apache.http.HttpEntity;
@@ -9,6 +10,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.InputStreamBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -30,10 +32,13 @@ public class PostFile {
 			}
 
 			FileBody bin = new FileBody(file, ContentType.DEFAULT_TEXT, "just-a-test.txt");
+			FileInputStream fis = new FileInputStream(file);
+			InputStreamBody isb = new InputStreamBody(fis,  ContentType.DEFAULT_TEXT);
 			StringBody comment = new StringBody("A binary file of some kind", ContentType.TEXT_PLAIN);
 
-			HttpEntity httpEntity = MultipartEntityBuilder.create().addPart("userfile", bin).addPart("comment", comment)
-					.build();
+			MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
+			entityBuilder.addPart("other", isb).addPart("comment", comment).addPart("userfile", bin);
+			HttpEntity httpEntity = entityBuilder.build();
 
 			postRequest.setEntity(httpEntity);
 
