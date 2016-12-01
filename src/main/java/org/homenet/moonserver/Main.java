2c72ea988ba29c008cb7650ab6d1d2a596d7ae2c
Lars Langhans <lars.langhans@gmx.de>
2016-12-01 08:38:35 +0100

    Abgestrippte Main

---
+++ src/main/java/org/homenet/moonserver/Main.java
@@ -0,0 +1,23 @@
+package org.homenet.moonserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+	
+	public Main() {
+		LOGGER.trace("c'tor main()");
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
