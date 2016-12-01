77437b5d62c9e843fc72932f7c483272265fe08a
Lars Langhans <lars.langhans@gmx.de>
2016-12-01 16:12:05 +0100

    the php files

--- src/main/resources/upload.php
+++ src/main/resources/upload.php
@@ -4,22 +4,22 @@ include "startendwith.php";
 
 print_r($_FILES);
 
-if (isset($_FILES['userfile']) && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-   if (endswith($_FILES['userfile']['name'], '.php')) die ("keine PHP Files!");
-   if (endswith($_FILES['userfile']['name'], '.phps')) die ("keine PHP Files!");
-   if (endswith($_FILES['userfile']['name'], '.php5')) die ("keine PHP Files!");
-   if (endswith($_FILES['userfile']['name'], '.php7')) die ("keine PHP Files!");
-   if (endswith($_FILES['userfile']['name'], '.phtml')) die ("keine PHP Files!");
-   if (endswith($_FILES['userfile']['name'], '.htm')) die ("keine HTML Files!");
-   if (endswith($_FILES['userfile']['name'], '.html')) die ("keine HTML Files!");
+if (isset($_FILES['privateuserfile']) && is_uploaded_file($_FILES['privateuserfile']['tmp_name'])) {
+   if (endswith($_FILES['privateuserfile']['name'], '.php')) die ("keine PHP Files!");
+   if (endswith($_FILES['privateuserfile']['name'], '.phps')) die ("keine PHP Files!");
+   if (endswith($_FILES['privateuserfile']['name'], '.php5')) die ("keine PHP Files!");
+   if (endswith($_FILES['privateuserfile']['name'], '.php7')) die ("keine PHP Files!");
+   if (endswith($_FILES['privateuserfile']['name'], '.phtml')) die ("keine PHP Files!");
+   if (endswith($_FILES['privateuserfile']['name'], '.htm')) die ("keine HTML Files!");
+   if (endswith($_FILES['privateuserfile']['name'], '.html')) die ("keine HTML Files!");
    
-   echo "File ". $_FILES['userfile']['name'] ." uploaded successfully.\n";
-  move_uploaded_file ($_FILES['userfile'] ['tmp_name'], $_FILES['userfile'] ['name']);
+   echo "File ". $_FILES['privateuserfile']['name'] ." uploaded successfully.\n";
+  move_uploaded_file ($_FILES['privateuserfile'] ['tmp_name'], $_FILES['privateuserfile'] ['name']);
 }
 else {
   echo "Possible file upload attack: ";
-  if (isset($_FILES['userfile'])) {
-     echo "filename '". $_FILES['userfile']['tmp_name'] . "'.";
+  if (isset($_FILES['privateuserfile'])) {
+     echo "filename '". $_FILES['privateuserfile']['tmp_name'] . "'.";
   }
   print_r($_FILES);
 }
